c8c1b7d570a98f7edaecd0c47159c87ecb7a9447
Sven Tschui <github@sventschui.ch>
2019-01-25 14:45:10 +0100

    Fix attributes directly on complex types

--- src/index.js
+++ src/index.js
@@ -370,10 +370,12 @@ commander
                   importPath,
                 });
               },
-              hasAttributes(children, options) {
-                if (children
-                && children.some(child => child.$ns.uri === 'http://www.w3.org/2001/XMLSchema'
-                  && ['attribute', 'anyAttribute', 'attributeGroup'].indexOf(child.$ns.local) !== -1)) {
+              hasAttributes(el, options) {
+                if (
+                  (el.$children && el.$children.some(child => child.$ns.uri === 'http://www.w3.org/2001/XMLSchema'
+                  && ['attribute', 'anyAttribute', 'attributeGroup'].indexOf(child.$ns.local) !== -1))
+                  || (el.$$parent.$children && el.$$parent.$children.some(child => child.$ns.uri === 'http://www.w3.org/2001/XMLSchema'
+                  && ['attribute', 'anyAttribute', 'attributeGroup'].indexOf(child.$ns.local) !== -1))) {
                   return options.fn(this);
                 }
 
